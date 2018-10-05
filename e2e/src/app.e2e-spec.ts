26bc40b444a9ba2be8ded6a61d30e2f8f267d404
ahmed <ahmedgh@rosafian.com>
2018-10-05 21:01:53 +0100

    first commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -9,6 +10,13 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
+    browser.get('https://angularjs.org')
+    browser.sleep(5000)
+console.log('fffffffff');
     expect(page.getParagraphText()).toEqual('Welcome to protractor!');
+  });
+
+   it('test2', () => {
+    expect(true).toBe(true)
   });
 });
