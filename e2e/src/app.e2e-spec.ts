2cabba1e4be6e3514ea7361e20dd46e94a733f7d
ahmed <ahmedgh@rosafian.com>
2018-10-05 22:55:00 +0100

    protractor by all earch ..etc

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -3,7 +3,23 @@ import { browser, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
+  let add = (a, b,type) => {
+    element(by.model('first')).sendKeys(a);
+    element(by.model('second')).sendKeys(b);
 
+    element.all(by.tagName('option')).each(item => {
+      item.getAttribute('value').then(value => {
+
+        if (value == type) {
+          item.click()
+        }
+      })
+
+    })
+
+    element(by.id('gobutton')).click()
+    browser.sleep(2000)
+  }
   beforeEach(() => {
     page = new AppPage();
   });
@@ -14,12 +30,23 @@ describe('workspace-project App', () => {
     browser.waitForAngularEnabled(false);
     browser.get('https://www.google.com')
     browser.get('http://juliemr.github.io/protractor-demo')
-    element(by.model('first')).sendKeys("5");
-    element(by.model('second')).sendKeys("5");
-    element(by.id('gobutton')).click()
-    browser.sleep(3000)
+    add("5", "7", "MULTIPLICATION")
+    add("5", "1", "DIVISION")
 
    
-    expect( element(by.css("h2[class='ng-binding']")).getText()).toBe("10")
+    browser.sleep(2000)
+
+    element.all(by.repeater('result in memory')).count().then(res => { console.log(res) })
+    element.all(by.repeater('result in memory')).each(ele => {
+      ele
+        .element(by.css('td:nth-child(3)')).getText()
+        .then(res => console.log(res))
+    })
+
+    // element(by.repeater('result in memory'))
+    //   .element(by.css('td:nth-child(3)')).getText()
+    //   .then(res => console.log(res))
+
+    // expect(element(by.css("h2[class='ng-binding']")).getText()).toBe("10")
   });
 });
